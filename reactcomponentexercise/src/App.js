157d7af5ada3d32c3d6a618e545f610863fdd44b
Jeeukrishnan <48768953+Jeeukrishnan@users.noreply.github.com>
2020-05-20 11:33:55 +0530

    Add files via upload

---
+++ reactcomponentexercise/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render()
+  { 
+  return (
+  	<div>
+
+        <Navbar dark color="primary">
+           <div className="container">
+              <NavbarBrand href="/">jeeu kayshyap</NavbarBrand>
+            </div>
+        </Navbar>
+
+       <Menu dishes={this.state.dishes}>
+       </Menu>
+     
+
+    </div>
+    );
+  
+}
+
+}
+  
+
+
+
+export default App;
